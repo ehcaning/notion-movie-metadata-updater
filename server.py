b3a2b8aa64cd0173731d9afef287c41227c5cb58
Ehsan Seyedi <git@ehcan.net>
2025-07-06 16:44:25 +0200

    Update movie metadata during bulk update process

--- server.py
+++ server.py
@@ -58,6 +58,7 @@ def create_app():
             changes = TvTimeProcessor(logger=logger).get_latest_changes(movies)
             for imdb_id, data in changes.items():
                 updater.upsert_movie_by_imdb_id(imdb_id, data)
+                updater.update_movie_metadata_by_imdb_id(imdb_id)
 
             logger.info(f"Successfully updated movies list")
             return JSONResponse({"status": "success"})
